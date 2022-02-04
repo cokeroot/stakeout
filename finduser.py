132e4a3e8052d436238a9b999e5c2b2b023272e6
jordan-dykhuizen <98981845+jordan-dykhuizen@users.noreply.github.com>
2022-02-03 18:13:56 -0700

    Updated file
    Added the functionality to tally up the total size of all the files located. Can be used as a starting point to weed out smaller files in the future if space becomes an issue.

--- finduser.py
+++ finduser.py
@@ -1,4 +1,5 @@
 import os
+import math
 
 # List files in the Users directory
 # To find out what the name of the device user is
@@ -6,6 +7,7 @@ import os
 path = "C://Users"
 users_list = os.listdir(path) # Lists directories from specified path
 path_of_users = [] # make a list to store the path of user
+totalSize = 0
 print(f"Files and directories inside {path}:")
 for dir in users_list:
     full_dir_path = os.path.join(path, dir)
@@ -46,9 +48,22 @@ def get_filepaths(directory):
 
     return file_paths
 
+def convert_size(size_bytes):
+    if size_bytes == 0:
+        return "0B"
+    size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+    i = int(math.floor(math.log(size_bytes, 1024)))
+    p = math.pow(1024,i)
+    s = round(size_bytes / p, 2)
+    return "%s %s" % (s, size_name[i])
+
 # print all the files found from walking through different User's DDD directories
 # The user's who don't have the DDD files won't be outputted.
 for path in interesting_dirs:
     full_file_paths = get_filepaths(path) # path is the starting path that the function will walk through
     for filepath in full_file_paths:
         print(filepath)
+        currentSize = os.path.getsize(filepath)
+        totalSize = totalSize + int(currentSize)
+
+print("Total size of all files located is: " + str(convert_size(totalSize)))
